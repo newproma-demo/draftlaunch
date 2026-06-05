987a1b67656c384ed165413c371346e192f8381e
newproma-demo <noguchi@sms-co.co.jp>
2026-06-05 15:05:49 +0900

    Update sidebar.js

--- js/sidebar.js
+++ js/sidebar.js
@@ -110,7 +110,6 @@ window.SHARED_SIDEBAR_HTML = `
     <ul class="submenu">
       <li><a href="keiyakushosofuiraikakunin.html">契約依頼・発注書作成</a></li>
       <li><a href="hatchujohoMsysemrenkei.html">発注情報Mｼｽﾃﾑ連携</a></li>
-      <li><a href="hatchurirekikensaku.html">発注履歴照会</a></li>
     </ul>
   </div>
 
